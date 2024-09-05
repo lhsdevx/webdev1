2b86471eb9831b81f3316bc503c25842e9420ee0
snek152 <kakanisnehil@gmail.com>
2024-09-04 18:35:43 -0700

    minor fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -76,7 +76,7 @@ export default function Home() {
 
       {/* Main Content */}
       <main className="bg-[#111113] text-white">
-        <section className="  max-w-7xl mx-auto flex flex-col lg:flex-row items-center justify-center gap-16 lg:gap-5 lg:items-start px-8 py-8 lg:py-20">
+        <section className="  max-w-7xl mx-auto flex flex-col lg:flex-row items-center justify-center gap-16 lg:gap-5 lg:items-start px-8 py-8 pb-0 lg:py-20 lg:pb-0">
           {/* Left Content Block */}
           <div className="flex flex-col gap-5 lg:gap-8 items-center justify-center text-center lg:text-left lg:items-start">
             <h1 className="font-extrabold text-4xl lg:text-6xl tracking-tight flex flex-col gap-2 items-center lg:items-start">
@@ -132,37 +132,44 @@ export default function Home() {
         </section>
 
         {/* heres a section???  */}
-        <section id="philosophy" className="flex flex-col md:flex-row items-center rounded-[60px]  overflow-hidden justify-center min-h-screen p-8">
+        <section
+          id="philosophy"
+          className="flex flex-col md:flex-row items-center rounded-[60px] overflow-hidden justify-center min-h-screen pt-0 p-8 pb-0"
+        >
+          <div className="md:w-1/3 p-8 md:p-0 lg:p-0 flex justify-center items-center">
+            <Image
+              src="/teaching.jpg"
+              alt="Inbox Preview"
+              width={735}
+              height={735}
+              className=" w-full h-auto max-w-sm max-h-sm rounded-lg"
+            />
+          </div>
 
-  <div className="md:w-1/3 p-8 md:p-0 lg:p-0 flex justify-center items-center">
-    <Image
-      src="/teaching.jpg"
-      alt="Inbox Preview"
-      width={735}
-      height={735}
-      className=" w-full h-auto max-w-sm max-h-sm rounded-lg"
-    />
-  </div>
-
-  <div className="md:w-1/2 p-8 text-center md:text-left">
-  <p className="text-2xl text-[#edeef0] pb-5 ">CORE PHILOSOPHY</p>
-    <h2 className="text-5xl font-bold mb-4 text-white">
-      Learning By Doing
-    </h2>
-    <p className="text-lg text-[#edeef0]  mb-6">
-    From learning important concepts to building powerful projects, everything we do is hands-on. We teach the basics of web development, mobile development, and more, and let members develop their skills by completing real-world projects. The unique approach we bring to teaching computer science and software development has empowered our members to build amazing and meaningful projects that contribute to the community.
-    </p>
-    <Button className="text-lg px-20 py-7 text-center btn-wide group bg-blue-700 hover:bg-blue-600 border-2 border-transparent hover:border-white flex gap-2 items-center justify-center text-white">
-                  <IterationCcw />
-                  Join Us!
-                </Button>
-  </div>
-
-</section>
+          <div className="md:w-1/2 p-8 text-center md:text-left">
+            <p className="text-2xl text-[#edeef0] pb-5 ">CORE PHILOSOPHY</p>
+            <h2 className="text-5xl font-bold mb-4 text-white">
+              Learning By Doing
+            </h2>
+            <p className="text-lg text-[#edeef0]  mb-6">
+              From learning important concepts to building powerful projects,
+              everything we do is hands-on. We teach the basics of web
+              development, mobile development, and more, and let members develop
+              their skills by completing real-world projects. The unique
+              approach we bring to teaching computer science and software
+              development has empowered our members to build amazing and
+              meaningful projects that contribute to the community.
+            </p>
+            <Button className="text-lg px-20 py-7 text-center btn-wide group bg-blue-700 hover:bg-blue-600 border-2 border-transparent hover:border-white flex gap-2 items-center justify-center text-white">
+              <IterationCcw />
+              Join Us!
+            </Button>
+          </div>
+        </section>
 
         <section
           id="team"
-          className="scroll-m-10 my-5 max-w-7xl mx-auto px-6 md:px-8 py-16"
+          className="scroll-m-10 py-0 my-0 max-w-7xl mx-auto px-6 md:px-8"
         >
           <h2 className="text-6xl font-bold text-center mb-12 bg-gradient-to-r from-cyan-300 to-sky-600 bg-clip-text text-transparent">
             Our Team
@@ -210,59 +217,131 @@ export default function Home() {
         </section>
         {/* heres a section???  */}
         <section className="my-14 max-w-7xl mx-auto px-6 md:px-8 py-16">
-          <h1 className="text-4xl font-bold text-center mb-12">
-            TECHNOLOGIES WE USE
-          </h1>
+          <h2 className="text-6xl font-bold text-center mb-2 bg-gradient-to-r from-cyan-300 to-sky-600 bg-clip-text text-transparent pb-5">
+            Technologies We Use
+          </h2>
           <div>
-            <div className="grid grid-cols-2 md:grid-cols-4 gap-8 mt-12">
+            <div className="grid grid-cols-2 sm:grid-cols-4 md:grid-cols-6 gap-8 mt-12">
               <div className="flex flex-col items-center">
-                <img src="javascript-logo.png" alt="Javascript Logo" className="w-20 h-20 object-contain" />
+                <Image
+                  src="/javascript-logo.png"
+                  alt="Javascript Logo"
+                  height={80}
+                  width={80}
+                  className="w-20 h-20 object-cover"
+                />
                 <h1 className="mt-4 text-center">Javascript</h1>
               </div>
               <div className="flex flex-col items-center">
-                <img src="typescript.png" alt="Typescript Logo" className="w-20 h-20 object-contain" />
+                <Image
+                  src="/typescript.png"
+                  alt="Typescript Logo"
+                  height={80}
+                  width={80}
+                  className="w-20 h-20 object-cover"
+                />
                 <h1 className="mt-4 text-center">Typescript</h1>
               </div>
               <div className="flex flex-col items-center">
-                <img src="html.png" alt="HTML5 Logo" className="w-20 h-20 object-contain" />
+                <Image
+                  src="/html.png"
+                  alt="HTML5 Logo"
+                  width={80}
+                  height={80}
+                  className="w-20 h-20 object-cover"
+                />
                 <h1 className="mt-4 text-center">HTML5</h1>
               </div>
               <div className="flex flex-col items-center">
-                <img src="css.png" alt="CSS3 Logo" className="w-20 h-20 object-contain" />
+                <Image
+                  src="/css.svg"
+                  alt="CSS3 Logo"
+                  height={80}
+                  width={80}
+                  className="w-20 h-20 object-cover"
+                />
                 <h1 className="mt-4 text-center">CSS3</h1>
               </div>
 
               <div className="flex flex-col items-center">
-                <img src="firebase.png" alt="firebase Logo" className="w-20 h-20 object-contain" />
-                <h1 className="mt-4 text-center">firebase</h1>
+                <Image
+                  src="/firebase.png"
+                  alt="firebase Logo"
+                  height={80}
+                  width={80}
+                  className="w-20 h-20 object-cover"
+                />
+                <h1 className="mt-4 text-center">Firebase</h1>
               </div>
               <div className="flex flex-col items-center">
-                <img src="figma.png" alt="figma Logo" className="w-20 h-20 object-contain" />
-                <h1 className="mt-4 text-center">figma</h1>
+                <Image
+                  src="/figma.png"
+                  alt="figma Logo"
+                  width={80}
+                  height={80}
+                  className="w-20 h-20 object-cover"
+                />
+                <h1 className="mt-4 text-center">Figma</h1>
               </div>
               <div className="flex flex-col items-center">
-                <img src="react.png" alt="react Logo" className="w-20 h-20 object-contain" />
-                <h1 className="mt-4 text-center">react</h1>
+                <Image
+                  height={80}
+                  width={80}
+                  src="/react.png"
+                  alt="react Logo"
+                  className="w-20 h-20 object-contain"
+                />
+                <h1 className="mt-4 text-center">React</h1>
               </div>
               <div className="flex flex-col items-center">
-                <img src="nextjs.jpg" alt="nextjs Logo" className="w-20 h-20 object-contain" />
-                <h1 className="mt-4 text-center">nextjs</h1>
+                <Image
+                  height={80}
+                  width={80}
+                  src="/nextjs.svg"
+                  alt="nextjs Logo"
+                  className="w-20 h-20 object-contain filter invert"
+                />
+                <h1 className="mt-4 text-center">Next.js</h1>
               </div>
               <div className="flex flex-col items-center">
-                <img src="supabase.jpg" alt="supabase Logo" className="w-20 h-20 object-contain" />
-                <h1 className="mt-4 text-center">supabase</h1>
+                <Image
+                  height={80}
+                  width={80}
+                  src="/supabase.png"
+                  alt="supabase Logo"
+                  className="w-20 h-20 object-contain"
+                />
+                <h1 className="mt-4 text-center">Supabase</h1>
               </div>
               <div className="flex flex-col items-center">
-                <img src="prisma.png" alt="prisma Logo" className="w-20 h-20 object-contain" />
-                <h1 className="mt-4 text-center">prisma</h1>
+                <Image
+                  height={80}
+                  width={80}
+                  src="/prisma.png"
+                  alt="prisma Logo"
+                  className="w-20 h-20 object-cover"
+                />
+                <h1 className="mt-4 text-center">Prisma</h1>
               </div>
               <div className="flex flex-col items-center">
-                <img src="svelte.png" alt="svelte Logo" className="w-20 h-20 object-contain" />
-                <h1 className="mt-4 text-center">svelte</h1>
+                <Image
+                  height={80}
+                  width={80}
+                  src="/svelte.png"
+                  alt="svelte Logo"
+                  className="w-20 h-20 object-contain"
+                />
+                <h1 className="mt-4 text-center">Svelte</h1>
               </div>
               <div className="flex flex-col items-center">
-                <img src="nodejs.webp" alt="nodejs Logo" className="w-20 h-20 object-contain" />
-                <h1 className="mt-4 text-center">nodejs</h1>
+                <Image
+                  height={80}
+                  width={80}
+                  src="/nodejs.png"
+                  alt="nodejs Logo"
+                  className="w-20 h-20 object-contain"
+                />
+                <h1 className="mt-4 text-center">Node.js</h1>
               </div>
             </div>
           </div>
